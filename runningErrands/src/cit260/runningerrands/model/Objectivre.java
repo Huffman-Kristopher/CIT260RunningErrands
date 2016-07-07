7a99d8bd94f5b7c4d79eed8d72324f5a28244254
BrownKirk <reddogfan2@gmail.com>
2016-07-06 20:51:39 -0500

    KBB - Second commit to correct merge conflicts

---
+++ runningErrands/src/cit260/runningerrands/model/Objectivre.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cit260.runningerrands.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Kristopher Huffman and Kirk Brown
+ */
+public class Objectivre implements Serializable{
+        
+    private Item item;
+    private int quantityNeeded;
+    private String description;
+    
+
+}
